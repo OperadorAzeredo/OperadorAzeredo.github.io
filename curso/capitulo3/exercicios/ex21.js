2f2aaa8113fcf112f6aaad3b9c8c998e8f17763d
OperadorAzeredo <lazaro.barbosa@live.com>
2021-12-10 20:34:09 -0300

    update

--- curso/capitulo3/exercicios/ex21.js
+++ curso/capitulo3/exercicios/ex21.js
@@ -1,7 +1,7 @@
 function change()
 {
-    (document.getElementById('lamp').src == "https://OperadorAzeredo.github.io/img/lampada_off.gif") ?
-     document.getElementById('lamp').src = "https://OperadorAzeredo.github.io/img/lampada_on.gif"
-    : document.getElementById('lamp').src = "https://OperadorAzeredo.github.io/img/lampada_off.gif";
+    (document.getElementById('lamp').src == "https://operadorazeredo.github.io/img/lampada_off.gif") ?
+     document.getElementById('lamp').src = "https://operadorazeredo.github.io/img/lampada_on.gif"
+    : document.getElementById('lamp').src = "https://operadorazeredo.github.io/img/lampada_off.gif";
     
 }
